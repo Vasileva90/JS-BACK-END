706bb171510720eead5bf4913df19b56acca0f0c
Vasileva90 <107758821+Vasileva90@users.noreply.github.com>
2023-11-17 12:24:28 +0200

    Create index.js

---
+++ Scaffold IP/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const handleBars = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes.js');
+const { authentication } = require('./middlewares/authMiddleware.js');
+
+const app = express();
+app.engine('hbs', handleBars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(authentication);
+app.use(routes);
+
+mongoose.set('strictQuery', false);
+// change to db name
+mongoose.connect('mongodb://127.0.0.1:27017/crypto'); //mongodb://localhost:27017/
+
+
+app.listen(3000, () => console.log('Server is listening on port 3000 ...'));
