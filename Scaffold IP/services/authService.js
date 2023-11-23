d10a1352268b6e5c96b229c10d7d8430b6f9b786
Vasileva90 <107758821+Vasileva90@users.noreply.github.com>
2023-11-23 12:21:30 +0200

    Create authService.js

---
+++ Scaffold IP/services/authService.js
@@ -0,0 +1,57 @@
+const User = require('../models/User.js');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jsonwebtoken.js');
+const {SECRET} = require('../constants.js');
+exports.isRegistered = (email) => User.findOne({ email });
+
+//LOGIN
+exports.login = async (email, password) => {
+
+    const user = await this.isRegistered(email);
+
+    if (!user) {
+        throw new Error('Must register first');
+    };
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid email or password!');
+    }
+
+    // generate token
+    const payload = {
+        _id: user._id,
+        email,
+        username: user.username
+    }
+
+    const token = await jwt.sign(payload, SECRET);
+
+    return token;
+
+};
+
+//REGISTER
+exports.register = async (username, email, password, repeatPassword) => {
+    const registeredUser = await this.isRegistered(email);
+
+
+    if (registeredUser) {
+        throw new Error('Email already registered!');
+    }
+
+    //validate password
+    if (password !== repeatPassword) {
+        throw new Error('Passwords don\'t match!');
+    }
+
+    //validate password according to the requirements
+    //дължина, брой символи и т.н.
+    if(password < 4) {
+        throw new Error('Password too short');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    await User.create({ username, email, password: hashedPassword });
+}
